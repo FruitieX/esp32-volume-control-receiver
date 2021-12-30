cb2226010503e38737f066828fb11f8f32fe4e98
Rasmus Lövegren <fruitiex@gmail.com>
2021-12-30 17:54:18 +0200

    Output via DS3502

--- main/espnow_example_main.c
+++ main/espnow_example_main.c
@@ -28,6 +28,61 @@
 #include "esp_now.h"
 #include "esp_crc.h"
 #include "espnow_example.h"
+#include "driver/i2c.h"
+#include "math.h"
+
+#define I2C_SCL_IO				22	             /*!< gpio number for I2C master clock */
+#define I2C_SDA_IO				21	             /*!< gpio number for I2C master data  */
+#define I2C_FREQ_HZ				400000           /*!< I2C master clock frequency */
+#define I2C_PORT_NUM			I2C_NUM_0        /*!< I2C port number for master dev */
+#define I2C_TX_BUF_DISABLE  	0                /*!< I2C master do not need buffer */
+#define I2C_RX_BUF_DISABLE  	0                /*!< I2C master do not need buffer */
+#define WRITE_BIT                          I2C_MASTER_WRITE /*!< I2C master write */
+#define ACK_CHECK_EN                       0x1              /*!< I2C master will check ack from slave*/
+#define ACK_VAL                            0x0              /*!< I2C ack value */
+#define NACK_VAL                           0x1              /*!< I2C nack value */
+
+#define DS3502_I2C_ADDR		0x28 // or 0x50?
+
+/**
+ * @brief Test code to write i2c slave device with registered interface
+ *        Master device write data to slave(both esp32),
+ *        the data will be stored in slave buffer.
+ *        We can read them out from slave buffer.
+ * ____________________________________________________________________________________
+ * | start | slave_addr + wr_bit + ack | register + ack | write n bytes + ack  | stop |
+ * --------|---------------------------|----------------|----------------------|------|
+ *
+ */
+static esp_err_t i2c_master_write_slave_reg(i2c_port_t i2c_num, uint8_t i2c_addr, uint8_t i2c_reg, uint8_t* data_wr, size_t size)
+{
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    // first, send device address (indicating write) & register to be written
+    i2c_master_write_byte(cmd, ( i2c_addr << 1 ) | WRITE_BIT, ACK_CHECK_EN);
+    // send register we want
+    i2c_master_write_byte(cmd, i2c_reg, ACK_CHECK_EN);
+    // write the data
+    i2c_master_write(cmd, data_wr, size, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+esp_err_t ds3502_write(uint8_t reg, uint8_t *pdata, uint8_t len) {
+    return i2c_master_write_slave_reg(I2C_PORT_NUM, DS3502_I2C_ADDR, reg, pdata, len);
+}
+
+static void ds3502_init() {
+    // Start by zeroing out the digipot, this also writes a new default value to the IVR
+    uint8_t val = 0x00;
+    ds3502_write(0x00, &(val), 1);
+
+    // Set MODE bit to 1 to disable writes to IVR
+    val = 0x80;
+    ds3502_write(0x02, &(val), 1);
+}
 
 #define ESPNOW_MAXDELAY 512
 
@@ -120,29 +175,49 @@ static void example_espnow_task(void *pvParameter)
                 ret = example_espnow_data_parse(recv_cb->data, recv_cb->data_len, &recv_state, &recv_seq, &recv_magic);
                 uint32_t payload = ((example_espnow_data_t *)recv_cb->data)->payload;
                 free(recv_cb->data);
-                if (ret == EXAMPLE_ESPNOW_DATA_BROADCAST) {
-                    ESP_LOGI(TAG, "Receive broadcast data from: "MACSTR", len: %d: %d", MAC2STR(recv_cb->mac_addr), recv_cb->data_len, payload);
 
-                    /* If MAC address does not exist in peer list, add it to peer list. */
-                    if (esp_now_is_peer_exist(recv_cb->mac_addr) == false) {
-                        esp_now_peer_info_t *peer = malloc(sizeof(esp_now_peer_info_t));
-                        if (peer == NULL) {
-                            ESP_LOGE(TAG, "Malloc peer information fail");
-                            vTaskDelete(NULL);
-                        }
-                        memset(peer, 0, sizeof(esp_now_peer_info_t));
-                        peer->channel = CONFIG_ESPNOW_CHANNEL;
-                        peer->ifidx = ESPNOW_WIFI_IF;
-                        peer->encrypt = true;
-                        memcpy(peer->lmk, CONFIG_ESPNOW_LMK, ESP_NOW_KEY_LEN);
-                        memcpy(peer->peer_addr, recv_cb->mac_addr, ESP_NOW_ETH_ALEN);
-                        ESP_ERROR_CHECK( esp_now_add_peer(peer) );
-                        free(peer);
+                if (ret == EXAMPLE_ESPNOW_DATA_BROADCAST) {
+                    // ESP_LOGI(TAG, "Receive broadcast data from: "MACSTR", len: %d: %d", MAC2STR(recv_cb->mac_addr), recv_cb->data_len, payload);
+
+                    // Scale value from [0, 4096[ to [0, 1[
+                    double x = payload / 4096.0;
+                    double val_d;
+
+                    // https://www.desmos.com/calculator/7jdp6fxktt
+                    if (x < 0.07)
+                    {
+                        val_d = 0.0 + x * 10.0;
+                    }
+                    else if (x < 0.11)
+                    {
+                        val_d = 0.7 + (x - 0.07) * 2.0;
+                    }
+                    else if (x < 0.15)
+                    {
+                        val_d = 0.78 + (x - 0.11) * 0.7;
+                    }
+                    else
+                    {
+                        val_d = 0.808 + (x - 0.15) * 0.2258823;
                     }
 
-                }
-                else if (ret == EXAMPLE_ESPNOW_DATA_UNICAST) {
-                    ESP_LOGI(TAG, "Receive %dth unicast data from: "MACSTR", len: %d", recv_seq, MAC2STR(recv_cb->mac_addr), recv_cb->data_len);
+                    // val_d = log_1000(x) + 1;
+                    // val_d = log(val_d) / log(1000.0) + 1.0;
+
+                    // Make sure val_d is non negative
+                    val_d = fmax(0.0, val_d);
+
+                    // Speaker & ear protection
+                    uint8_t max_val = 95;
+
+                    // Scale value from [0, 1[ to [0, max_val]
+                    uint8_t val = val_d * (double) max_val;
+                    val = fmin(max_val, val);
+
+                    printf("x: %f, val_d: %f, val: %d\n", x, val_d, val);
+
+                    // ESP_LOGI(TAG, "Writing value: %d", val);
+                    ds3502_write(0x00, &(val), 1);
                 }
                 else {
                     ESP_LOGI(TAG, "Receive error data from: "MACSTR"", MAC2STR(recv_cb->mac_addr));
@@ -220,8 +295,28 @@ static esp_err_t example_espnow_init(void)
     return ESP_OK;
 }
 
-void app_main(void)
-{
+static void i2c_master_init() {
+    // Init I2C
+    int i2c_master_port = I2C_PORT_NUM;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_SDA_IO,         // select GPIO specific to your project
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_SCL_IO,         // select GPIO specific to your project
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_FREQ_HZ,  // select frequency specific to your project
+        // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
+    };
+    i2c_param_config(i2c_master_port, &conf);
+
+    i2c_driver_install(i2c_master_port, conf.mode,
+    		I2C_RX_BUF_DISABLE, I2C_TX_BUF_DISABLE, 0);
+}
+
+void app_main(void) {
+    i2c_master_init();
+    ds3502_init();
+
     // Initialize NVS
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
